2b1718c823b42a9d36fdb9992d47b8193f36aefb
kundan <girikundan553@gmail.com>
2022-06-26 13:22:32 +0545

    addstudentregister

---
+++ src/constants/index.js
@@ -0,0 +1,26 @@
+export const STUDENTS = [
+    {   
+        id: 1,
+        name: "John Smith",
+        dob: '1999-8-9',
+        address: 'Lalitpur',
+    },
+    {   
+        id: 2,
+        name: "George Everest",
+        dob: '1999-3-12',
+        address: 'Lalitpur',
+    },
+    {   
+        id: 3,
+        name: "Dinesh Singh Mahat",
+        dob: '1999-2-9',
+        address: 'Siraha',
+    },
+    {   
+        id: 4,
+        name: "Anup Smith",
+        dob: '1998-8-9',
+        address: 'Dhangadhi',
+    },
+]
