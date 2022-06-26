4d65f205f9f7e845c9cc3883e001f87de3f5d8d4
kundan <girikundan553@gmail.com>
2022-06-26 14:24:35 +0545

    hovered

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,17 +1,28 @@
-import React,{useState} from "react";
-const Counter=()=>{
-const[count, setCount]=useState(0)
-return(
-<div>
+import React, { useState } from "react";
+import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/ai";
 
-<button
-onClick={() =>setCount(count + 1)}>+</button>
-<h1>{count}</h1>
+const Counter = () => {
+  const [count, setCount] = useState(1);
+  const [hovered, setHovered] = useState('');
 
-<button
-onClick={()=>setCount(count>0? count -1:count)}>-</button>
-</div>
+  return (
+    <div>
+      <AiOutlinePlusCircle
+        size={count + 24}
+        color={count >= 12 ? "red" : `${hovered === 'plus' ? "gold" : "green"}`}
+        onMouseOver={(e) => setHovered('plus')}
+        onClick={() => setCount(count >= 12 ? count : count + 1)}
+      />
+      <h1 style={{ color: `${count >= 12 ? "red" : "pink"}` }}>{count}</h1>
+      <AiOutlineMinusCircle
+        size={count + 24}
+        color={count >= 12 ? "red" : `${hovered === 'minus' ? "gold" : "green"}`}
+        onMouseOver={(e) => setHovered('minus')}
+        onClick={() => setCount(count > 1 ? count - 1 : count)}
+      />
+      <button onClick={() => setCount(count > 1 ? count - 1 : count)}>-</button> 
+    </div>
+  );
+};
 
-)
-}
 export default Counter;
